f384f327e0f081fc5002f22489407562ad6a08a1
Abhishek Santra <abhisheksantra28@gmail.com>
2023-09-30 19:29:21 +0530

    updated

--- NodeAPI/routes/user.js
+++ NodeAPI/routes/user.js
@@ -5,10 +5,8 @@ import { isAuthenticated } from "../middlewares/auth.js";
 const router = express.Router();
 
 router.post("/register", register);
-
 router.post("/login", login);
 router.get("/logout", logout);
-
 router.get("/me", isAuthenticated, getMyProfile);
 
 export default router;
