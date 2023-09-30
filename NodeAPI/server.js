82261d54e2e642a5a767f170d984d6478ed8a52b
Abhishek Santra <abhisheksantra28@gmail.com>
2023-09-30 15:41:22 +0530

    Update MondoAtlas URI

--- NodeAPI/server.js
+++ NodeAPI/server.js
@@ -3,6 +3,9 @@ import { connectDB } from "./data/database.js";
 
 connectDB();
 
+
 app.listen(process.env.PORT, () => {
   console.log(`Server is working on Port ${process.env.PORT} in ${process.env.NODE_ENV} Mode`);
 });
+
+
